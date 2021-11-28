dd30a9820a02f467dbab066161832e982baf9bcb
Samin Sarker <saminsarker@Samins-MBP.lan>
2021-11-28 15:39:39 -0500

    map added

---
+++ Map.java
@@ -0,0 +1,33 @@
+import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.awt.Point;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Map {
+
+  BufferedImage image;
+  Point position;
+
+  public Map() {
+    loadGraphics();
+    position = new Point(0,0);
+  }
+
+  public void loadGraphics() {
+    try {
+      image = ImageIO.read(new File("images/map.png"));
+    }
+    catch (IOException exc) {
+      System.out.println("no open");
+    }
+  }
+
+  public void show(Graphics g, ImageObserver observer) {
+    g.drawImage(image, position.x, position.y, observer);
+  }
+
+}
